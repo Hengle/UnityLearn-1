3e08a01781e447a27b20c2d50ee8100905af15bd
王祖雄 <wangzuxiong@galasports.net>
2019-04-24 17:45:44 +0800

    UGUI按钮可点击区域测试

---
+++ Assets/ClickButtonTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ClickButtonTest : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        this.transform.GetComponent<Button>().onClick.AddListener(OnButton);
+    }
+
+    private void OnButton()
+    {
+        Debug.Log(">>>>OnButton");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    private void Awake()
+    {
+        Debug.Log("Awake");
+    }
+    private void OnEnable()
+    {
+        Debug.Log("OnEnable");
+    }
+
+    void Start()
+    {
+        Debug.Log("Start");
+
+
+        Dictionary<string, int> keyValuePairs = new Dictionary<string, int>();
+
+        var enumerator = keyValuePairs.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            var current = enumerator.Current;
+            Debug.Log(current.Key + "_" + current.Value);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
