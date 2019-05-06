56a336ebb4db8698ae607b998cc7848f7d9e4810
王祖雄 <wangzuxiong@galasports.net>
2019-05-06 18:11:57 +0800

    修改readme

---
+++ Assets/Lab.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lab : MonoBehaviour
+{
+    private List<string> _strList;
+
+    private void Start()
+    {
+        _strList = new List<string>();
+
+        for (int i = 0; i < 99999; i++)
+        {
+            _strList.Add("123123");
+        }
+
+        Debug.Log("List 初始化完成");
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("释放List");
+    }
+}
