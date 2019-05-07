a0c002100a4ca670cda7c547f8ff811d695dd78e
王祖雄 <wangzuxiong@galasports.net>
2019-05-07 16:35:34 +0800

    MessageManager

---
+++ Assets/Script/Service/MessageManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MessageManager : MonoBehaviour
+{
+    private readonly string BASE_URL = "";
+
+    public void Request<T, U>(string cmd, T input, Action<U> callBack)
+        where T : BaseInputVO
+        where U : BaseOutputVO
+    {
+        string url = BASE_URL + cmd;
+
+        StartCoroutine(RequestEnumerator(url, input, callBack));
+    }
+
+
+    private IEnumerator RequestEnumerator<T, U>(string url, T input, Action<U> callBack)
+          where T : BaseInputVO
+          where U : BaseOutputVO
+    {
+        //请求头
+        Dictionary<string, string> headers = new Dictionary<string, string>();
+        byte[] postData = new byte[10];
+
+        //Protobuf转化为string
+        //string转化为byte数组
+        //加密
+
+        WWW www = new WWW(url, postData, headers);
+        //UnityWebRequest unityWebRequest = new UnityWebRequest(,)
+        yield return www;
+
+        //校验
+        //判断
+        string text = www.text;
+        //将text转化为U
+        //callBackAction(u);
+        www.Dispose();
+    }
+
+}
+
+
+public class BaseInputVO
+{
+
+}
+
+public class BaseOutputVO
+{
+
+}
+
+public class CMD
+{
+    public static readonly string LOGIN = "";
+}
