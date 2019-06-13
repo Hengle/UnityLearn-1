adade7077cf07e7189c87876e2f308894ff05d05
王祖雄 <wangzuxiong@galasports.net>
2019-06-13 10:33:51 +0800

    增加公共UI组件

--- Assets/Lab.cs
+++ Assets/Lab.cs
@@ -14,4 +14,13 @@ public class Lab : MonoBehaviour
     {
        
     }
+    private void Start()
+    {
+        this.transform.GetComponent<Toggle>().onValueChanged.AddListener(OnToggle);
+    }
+
+    private void OnToggle(bool arg0)
+    {
+        Debug.Log(arg0);
+    }
 }
