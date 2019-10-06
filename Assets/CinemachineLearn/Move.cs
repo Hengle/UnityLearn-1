e9b029d87ffc90d0c127d89f47447c02ec1d4c01
WangZuXiong <32264952+WangZuXiong@users.noreply.github.com>
2019-10-06 11:32:03 +0800

    cinemachine && Timeline learn

---
+++ Assets/CinemachineLearn/Move.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float Speed = 1;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * Speed);
+    }
+}
